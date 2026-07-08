de5143035cc75d5d18685f5f62cfa1dcc1e85722
Gourav Kumar <gouravkumar090720@gmail.com>
2026-07-08 21:45:48 +0530

    Time: 0 ms (100%), Space: 8 MB (25.97%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int bitwiseComplement(int num) 
+    {
+         // EDGE CASE
+        if(num ==0)
+        return 1 ; 
+
+        // First convert in binary--> change bit --> convert to integer
+
+        int long mul = 1 , ans = 0 ; 
+        while(num)
+        {
+            int rem = num%2 ;       // remainder
+            rem = rem^1;            // bit changed
+            ans =  rem*mul +ans ;   // converting in decimal again
+            mul = mul*2;            // update the mul 
+            num = num /2 ;          // updating number
+        }
+
+        return ans;
+    
+
+    // mul signed int overflow de rha tha to usko long int bana diye
+    }
+};
