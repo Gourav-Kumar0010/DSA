25f273b0b7753f885d38221b65dd69cb06beb045
Gourav Kumar <gouravkumar090720@gmail.com>
2026-07-20 21:29:26 +0530

    Add method to reverse linked list using vector
    Implemented a method to reverse a linked list by storing values in a vector and updating nodes.

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -32,3 +32,36 @@ public:
         
     }
 };
+
+// Method 01
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        // Reversing only element not the actual node
+
+       if(head==NULL)
+        return head;
+
+        ListNode *curr;
+        curr = head ;
+        vector<int> arr ;
+
+        while(curr!=NULL)
+        {
+            arr.push_back(curr->val);
+            curr=curr->next;
+        }
+        //vector me sara value aa gya 
+        curr = head;
+        int i = arr.size()-1; //last ko point kar rha h 
+        while(i>=0)
+        {
+            curr->val=arr[i];
+            curr=curr->next;
+            i--;
+        }
+        return head;
+        
+    }
+};
